af623f79a0f782c7dd3b03dabf7adde18b5adc18
Jaret Peerson <contact@jaretpeerson.com>
2025-09-14 21:28:52 -0400

    Fine-Tuned Transitions

--- expandCollapse/expandCollapse.js
+++ expandCollapse/expandCollapse.js
@@ -1,8 +1,27 @@
 document.addEventListener("click", (e) => {
   const icon = e.target.closest(".plus-icon")
-  if (!icon) return // only run when .plus-icon is clicked
+  if (!icon) return
 
   const body = icon.parentElement.nextElementSibling
 
-  body.classList.toggle("show")
+  if (body.classList.contains("show")) {
+    // Expand
+    body.style.height = body.scrollHeight + "px"
+    body.offsetHeight
+    body.style.height = "0"
+    body.classList.remove("show")
+    icon.classList.remove("active")
+  } else {
+    // Expand
+    body.style.height = body.scrollHeight + "px"
+    body.classList.add("show")
+    icon.classList.add("active")
+    body.addEventListener(
+      "transitionend",
+      () => {
+        body.style.height = "auto"
+      },
+      { once: true }
+    )
+  }
 })
