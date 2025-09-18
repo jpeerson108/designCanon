12a3c53ea6a130f53543fac444256e72a17c3bd9
Jaret Peerson <contact@jaretpeerson.com>
2025-09-18 17:04:55 -0400

    Building the JS - Not done yet

---
+++ hoverButtonTest/hoverButtonTest.js
@@ -0,0 +1,6 @@
+const btnWrapper = document.querySelectorAll(".btn-wrapper")
+const btnIcon = document.querySelectorAll(".btn-icon")
+
+btnWrapper.addEventListener("mouseenter", (e) => {
+  btnIcon.classList.add("")
+})
