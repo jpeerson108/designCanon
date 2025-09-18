9b567ea2e11008376786a54daf11fab07c40b49f
Jaret Peerson <contact@jaretpeerson.com>
2025-09-17 21:43:47 -0400

    Nav Menu is screwed up - still working on it.

--- designCanon/globalNav/globalNav.js
+++ designCanon/globalNav/globalNav.js
@@ -1,27 +1,27 @@
-// const navAboutText = document.getElementById("about-text")
-// const navAboutIcon = document.getElementById("about-icon")
+const navAboutText = document.getElementById("about-text")
+const navAboutIcon = document.getElementById("about-icon")
 // const navCheatSheetsText = document.getElementById("cheatSheets-text")
 // const navCheatSheetsIcon = document.getElementById("cheatSheets-icon")
 // const navTutorialsText = document.getElementById("tutorials-text")
 // const navTutorialsIcon = document.getElementById("tutorials-icon")
 
-const aboutWrapper = document.querySelector(".about-wrapper")
+// const aboutWrapper = document.querySelector(".about-wrapper")
 
-aboutWrapper = addEventListener("mouseenter", (e) => {
-  aboutWrapper.classList.add("show-nav-text")
-})
-
-aboutWrapper = addEventListener("mouseleave", (e) => {
-  aboutWrapper.classList.remove("show-nav-text")
-})
-
-// navAboutIcon.addEventListener("mouseenter", (e) => {
-//   navAboutText.classList.add("show-nav-text")
+// aboutWrapper = addEventListener("mouseenter", (e) => {
+//   aboutWrapper.classList.add("show-nav-text")
 // })
 
-// navAboutIcon.addEventListener("mouseleave", (e) => {
-//   navAboutText.classList.remove("show-nav-text")
+// aboutWrapper = addEventListener("mouseleave", (e) => {
+//   aboutWrapper.classList.remove("show-nav-text")
 // })
+
+navAboutIcon.addEventListener("mouseenter", (e) => {
+  navAboutText.classList.add("show-nav-text")
+})
+
+navAboutIcon.addEventListener("mouseleave", (e) => {
+  navAboutText.classList.remove("show-nav-text")
+})
 
 // navCheatSheetsIcon.addEventListener("mouseenter", (e) => {
 //   navCheatSheetsText.classList.add("show-nav-text")
