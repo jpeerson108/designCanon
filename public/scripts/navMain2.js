82db2963a92649273d01820070982a2537255de0
Jaret Peerson <contact@jaretpeerson.com>
2025-09-30 18:33:09 -0400

    navbar2: dropdown details, hide on scroll, etc.

--- public/scripts/navMain2.js
+++ public/scripts/navMain2.js
@@ -35,3 +35,16 @@ menuBox.forEach((box) => {
     }
   }
 })
+
+// Hide navbar on scroll-down, show on scroll-up
+let lastScrollY = window.scrollY
+const navbar = document.querySelector("nav")
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > lastScrollY) {
+    navbar.classList.add("hidden")
+  } else {
+    navbar.classList.remove("hidden")
+  }
+  lastScrollY = window.scrollY
+})
