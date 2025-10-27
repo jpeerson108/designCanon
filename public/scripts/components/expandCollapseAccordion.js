fc8cf639f674957ac567516a3904473826075fb5
Jaret Peerson <contact@jaretpeerson.com>
2025-10-27 19:55:45 -0400

    Updated styling

--- public/scripts/components/expandCollapseAccordion.js
+++ public/scripts/components/expandCollapseAccordion.js
@@ -1,6 +1,24 @@
-document.addEventListener("click", (e) => {
-  const icon = e.target.closest(".plus-icon")
+document.addEventListener("click", (event) => {
+  const icon = event.target.closest(".plus-icon")
   if (!icon) return
+
+  const allBodies = document.querySelectorAll(".show")
+  const allIcons = document.querySelectorAll(".plus-icon.active")
+
+  allBodies.forEach((body) => {
+    if (body !== icon.parentElement.nextElementSibling) {
+      body.style.height = body.scrollHeight + "px"
+      body.offsetHeight
+      body.style.height = "0"
+      body.classList.remove("show")
+    }
+  })
+
+  allIcons.forEach((activeIcon) => {
+    if (activeIcon !== icon) {
+      activeIcon.classList.remove("active")
+    }
+  })
 
   const body = icon.parentElement.nextElementSibling
 
