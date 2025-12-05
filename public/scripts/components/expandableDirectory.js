84bc30a43c449844f13b336a0afd78d58711cfd5
Jaret Peerson <contact@jaretpeerson.com>
2025-12-05 14:27:43 -0500

    Added expand collapse functionality

--- public/scripts/components/expandableDirectory.js
+++ public/scripts/components/expandableDirectory.js
@@ -1,2 +1,13 @@
-const dropdownSelector = document.querySelector("dropdown-selector")
-const closeIcon = document.querySelector("top-bar-close-icon")
+const dialogueBoxWrapper = document.querySelector(".dialogue-box-wrapper")
+const dropdownSelector = document.querySelector(".dropdown-selector")
+const closeIcon = document.querySelector(".top-bar-close-icon")
+
+dropdownSelector.addEventListener("click", () => {
+  dialogueBoxWrapper.classList.remove("inactive")
+  dropdownSelector.classList.add("inactive")
+})
+
+closeIcon.addEventListener("click", () => {
+  dialogueBoxWrapper.classList.add("inactive")
+  dropdownSelector.classList.remove("inactive")
+})
