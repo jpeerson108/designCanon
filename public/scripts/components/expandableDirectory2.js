f58669a8147b6ce263044638215a68cc824c8fde
Jaret Peerson <contact@jaretpeerson.com>
2025-12-12 19:27:38 -0500

    Working commit

--- public/scripts/components/expandableDirectory2.js
+++ public/scripts/components/expandableDirectory2.js
@@ -1,6 +1,9 @@
 const dialogueBox = document.querySelector(".dialogue-box")
 const dropdownSelector = document.querySelector(".dropdown-selector")
 const closeIcon = document.querySelector(".top-bar-close-icon")
+const dropdownCount = document.querySelector(".dropdown-count")
+const avatarListItems = document.querySelectorAll(".avatars ul li")
+const additionalCount = avatarListItems.length - 4
 
 dropdownSelector.addEventListener("click", () => {
   dialogueBox.classList.remove("inactive")
@@ -11,3 +14,7 @@ closeIcon.addEventListener("click", () => {
   dialogueBox.classList.add("inactive")
   dropdownSelector.classList.remove("inactive")
 })
+
+if (additionalCount > 0) {
+  dropdownCount.textContent = `+${additionalCount}`
+}
