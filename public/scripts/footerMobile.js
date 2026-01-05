8d003b518b2649937615c5eaab746d81a9eab502
Jaret Peerson <contact@jaretpeerson.com>
2026-01-05 14:07:05 -0500

    Adding functionality and animations to mobile footer menu

--- public/scripts/footerMobile.js
+++ public/scripts/footerMobile.js
@@ -1,1 +1,6 @@
-// Placeholder
+const footerMobilePieMenu = document.getElementById("footer-mobile-pie-menu")
+const footerMobileWrapper = document.querySelector(".footer-mobile-wrapper")
+
+footerMobilePieMenu.addEventListener("click", () => {
+  footerMobileWrapper.classList.toggle("active")
+})
