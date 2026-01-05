8e4bac10a6bdbc808602cf4d9afb111ff0ae679d
Jaret Peerson <contact@jaretpeerson.com>
2026-01-05 14:22:05 -0500

    Mostly functional at this point

--- public/scripts/footerMobile.js
+++ public/scripts/footerMobile.js
@@ -1,6 +1,18 @@
 const footerMobilePieMenu = document.getElementById("footer-mobile-pie-menu")
 const footerMobileWrapper = document.querySelector(".footer-mobile-wrapper")
+const footerMobileBackdrop = document.querySelector(".footer-mobile-backdrop")
+const footerMobilePieMenuText = document.querySelector(
+  ".footer-mobile-item.pie-menu .footer-mobile-text"
+)
 
 footerMobilePieMenu.addEventListener("click", () => {
   footerMobileWrapper.classList.toggle("active")
+})
+
+footerMobileBackdrop.addEventListener("click", () => {
+  footerMobileWrapper.classList.remove("active")
+})
+
+footerMobilePieMenuText.addEventListener("click", () => {
+  footerMobileWrapper.classList.remove("active")
 })
