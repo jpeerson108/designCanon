5e93d9176c86360927e5b9478a6aad13ed48b2cc
Jaret Peerson <contact@jaretpeerson.com>
2026-01-20 19:39:23 -0500

    Moved main links to global config file

--- src/config.ts
+++ src/config.ts
@@ -1,1 +1,14 @@
 export const SITE_NAME = "Shapeshift";
+
+// Navigation Links
+export const NAV_LINKS = {
+  home: "/",
+  inspiration: "/inspiration",
+  buttons: "/buttons",
+  github: "https://github.com/jpeerson108",
+  twitter: "",
+  linkedin: "https://www.linkedin.com/in/jaret-peerson/",
+  instagram: "",
+  youtube: "",
+  email: "mailto:hello@shapeshift.supply?subject=Inquiry from Shapeshift Supply",
+} as const;
